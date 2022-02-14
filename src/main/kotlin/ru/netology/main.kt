85ea50c9bc52cdb13d55b981f28fafa835e570db
Ilya <onischuk2007@ya.ru>
2022-02-14 14:23:15 +0300

    Commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,47 +1,51 @@
 package ru.netology
 
+
 fun main() {
-    val time = 1000
-    val minute = time / 60
-    val hours = (time / 60) / 60
-    val result = agoToText(time, minute, hours)
+    val amountSeconds = 10000
+    val oneMinute = 60
+    val oneHour = oneMinute * 60
+    val oneDay = oneHour * 24
+    val amountMinutes = amountSeconds / oneMinute
+    val amountHours = amountMinutes / oneMinute
+    val result = agoToText(amountSeconds, amountMinutes, amountHours, oneMinute, oneHour, oneDay)
     println("пользователь был $result")
 }
 
-fun agoToText(time: Int, minute: Int = time / 60, hours: Int = (time / 60) / 60): String {
-    val convertMinutes = textBeAppTooConvertMinutes(time, minute)
-    val convertHours = textBeAppTooConvertHours(time, hours)
+fun agoToText(amountSeconds: Int, amountMinutes: Int, amountHours: Int, oneMinute: Int, oneHour: Int, oneDay: Int): String {
+    val convertMinutes = convertingMinutesToText(amountMinutes)
+    val convertHours = convertingHoursToText(amountHours)
 
-    val agoToText = when(time) {
-        in 0..60 -> "только что"
-        in 61..60 * 60 -> "$minute $convertMinutes назад"
-        in 60 * 60 + 1..24 * 60 * 60 -> "$hours $convertHours назад"
-        in 24 * 60 * 60 + 1..(24 * 60 * 60) * 2 -> "сегодня"
-        in ((24 * 60 * 60) * 2) + 1..(24 * 60 * 60) * 3 -> "вчера"
+    val agoToText = when(amountSeconds) {
+        in 0..oneMinute -> "только что"
+        in oneMinute + 1..oneHour -> "$amountMinutes $convertMinutes назад"
+        in oneHour + 1..oneDay -> "$amountHours $convertHours назад"
+        in oneDay + 1..oneDay * 2 -> "сегодня"
+        in oneDay * 2 + 1..oneDay * 3 -> "вчера"
         else -> "давно"
     }
     return agoToText
 }
 
-fun textBeAppTooConvertMinutes(time: Int, minute: Int = time / 60): String {
+fun convertingMinutesToText(amountMinutes: Int): String {
 
-    val textBeAppTooConvertMinutes = when(minute) {
+    val convertingMinutesToText = when(amountMinutes) {
        1, 21, 31, 41, 51 -> "минуту"
        in 2..4, in 22..24, in 32..34, in 42..44, in 52..54 -> "минуты"
        in 5..20, in 25..30, in 35..40, in 45..50, in 55..60 -> "минут"
        else -> "ошибка"
     }
-    return textBeAppTooConvertMinutes
+    return convertingMinutesToText
 
 }
-fun textBeAppTooConvertHours(time: Int, hours: Int = (time / 60) / 60): String {
-    val textBeAppTooConvertHours = when(hours) {
+fun convertingHoursToText(amountHours: Int): String {
+    val convertingHoursToText = when(amountHours) {
         1, 21 -> "час"
         in 2..4, in 22..24 -> "часа"
         in 5..20 -> "часов"
         else -> "ошибка"
     }
-    return textBeAppTooConvertHours
+    return convertingHoursToText
 }
 
 
