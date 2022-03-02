1d5e27d9c03c840c83b309e5437004ee6404b758
Ilya <onischuk2007@ya.ru>
2022-03-02 08:42:48 +0300

    Commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -5,29 +5,32 @@ const val oneHour = 3600
 const val oneDay = 86400
 
 fun main() {
-    val amountSeconds = 80000
+    val amountSeconds = 500
     val result = agoToText(amountSeconds)
     println("Пользователь был $result")
 }
 
 fun agoToText(amountSeconds: Int): String {
-    val amountMinutes = amountSeconds / oneMinute
-    val amountHours = amountSeconds / oneHour
-    val convertMinutes = convertingMinutesToText(amountSeconds)
-    val convertHours = convertingHoursToText(amountSeconds)
 
     return when(amountSeconds) {
         in 0..oneMinute -> "только что"
-        in oneMinute + 1..oneHour -> "$amountMinutes $convertMinutes назад"
-        in oneHour + 1..oneDay -> "$amountHours $convertHours назад"
+        in oneMinute + 1..oneHour -> {
+            val amountMinutes = amountSeconds / oneMinute
+            val convertMinutes = convertingMinutesToText(amountMinutes)
+            "$amountMinutes $convertMinutes назад"
+        }
+        in oneHour + 1..oneDay -> {
+            val amountHours = amountSeconds / oneHour
+            val convertHours = convertingHoursToText(amountHours)
+            "$amountHours $convertHours назад"
+        }
         in oneDay + 1..oneDay * 2 -> "сегодня"
         in oneDay * 2 + 1..oneDay * 3 -> "вчера"
         else -> "давно"
     }
 }
 
-fun convertingMinutesToText(amountSeconds: Int): String {
-    val amountMinutes = amountSeconds / oneMinute
+fun convertingMinutesToText(amountMinutes: Int): String {
     val remainsMinutesTen: Int = amountMinutes % 10
     val remainsMinutesHundred: Int = amountMinutes % 100
 
@@ -38,8 +41,7 @@ fun convertingMinutesToText(amountSeconds: Int): String {
     }
 }
 
-fun convertingHoursToText(amountSeconds: Int): String {
-    val amountHours = amountSeconds / oneHour
+fun convertingHoursToText(amountHours: Int): String {
     val remainsHoursTen: Int = amountHours % 10
     val remainsHoursHundred: Int = amountHours % 100
 
@@ -50,5 +52,4 @@ fun convertingHoursToText(amountSeconds: Int): String {
     }
 }
 
-// Доработка: Сообразил с остатком от деления
 
